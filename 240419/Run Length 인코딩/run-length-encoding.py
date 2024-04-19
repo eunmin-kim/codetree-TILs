c8c39757a3fb9f8a5880c71710c097857a1b657a
SK <68364467+eunmin-kim@users.noreply.github.com>
2024-04-19 15:25:34 +0900

    Title: Run Length 인코딩, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 240419/Run Length 인코딩/run-length-encoding.py
+++ 240419/Run Length 인코딩/run-length-encoding.py
@@ -25,8 +25,14 @@ num_ans.append(cnt)
 # a_str = len(str(word_ans[0]) + str(num_ans[0]))
 # print(f"{a_str}")
 first = 0
+
+ans_str = ""
 for i in range(len(word_ans)):
-    if first == 0:
-        print(f"{len(str(word_ans[i])+str(num_ans[i]))}")
-        first += 1
-    print(f"{word_ans[i]}{num_ans[i]}",end="")
+    # if first == 0:
+    #     print(f"{len(str(word_ans[i])+str(num_ans[i]))}")
+    #     first += 1
+    ans_str += str(word_ans[i])+str(num_ans[i])
+    # print(f"{word_ans[i]}{num_ans[i]}",end="")
+
+print(len(ans_str))
+print(ans_str)
