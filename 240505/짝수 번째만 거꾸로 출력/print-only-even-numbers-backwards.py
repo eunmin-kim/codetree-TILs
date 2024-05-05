344ef06681f497a232528d6c2e1e2ec2515ae178
SK <68364467+eunmin-kim@users.noreply.github.com>
2024-05-05 15:30:58 +0900

    Title: 짝수 번째만 거꾸로 출력, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240505/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.py
+++ 240505/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.py
@@ -1,4 +1,12 @@
 a = input()
 
-for i in range(len(a)-1,-1,-2):
-    print(f"{a[i]}",end="")
+# for i in range(len(a)-1,-1,-2):
+#     print(f"{a[i]}",end="")
+ans = []
+
+for i in range(1,len(a)+1,1):
+    if i % 2 == 0:
+        ans.append(a[i-1])
+
+for i in range(len(ans)-1,-1,-1):
+    print(f"{ans[i]}")
