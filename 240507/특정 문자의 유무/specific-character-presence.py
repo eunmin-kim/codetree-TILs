c4e331cbfceff5c0d883c47d8c6ccde4c37a6fff
SK <68364467+eunmin-kim@users.noreply.github.com>
2024-05-07 18:49:18 +0900

    Title: 특정 문자의 유무, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240507/특정 문자의 유무/specific-character-presence.py
+++ 240507/특정 문자의 유무/specific-character-presence.py
@@ -8,6 +8,6 @@ for i in range(len(a)-1):
     if a[i] == b[0] and a[i+1] == b[1]:
         exists = "Yes"
     if a[i] == c[0] and a[i+1] == c[1]:
-        all_same = "No"
+        all_same = "Yes"
 
 print(f"{exists} {all_same}")
